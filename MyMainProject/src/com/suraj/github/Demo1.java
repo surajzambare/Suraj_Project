36cb10a29562906fffa604f0aa9324099ae79f62
surajzambare <surajzambare9@gmail.com>
2022-09-14 10:36:53 +0530

    first commit

---
+++ MyMainProject/src/com/suraj/github/Demo1.java
@@ -0,0 +1,18 @@
+package com.suraj.github;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		if(a>b) {
+			System.out.println("a is greater");
+		}else if(b>a){
+			System.out.println("a is smaller");
+		}else {
+			System.out.println("a & b are equal");
+		}
+
+	}
+
+}
---
+++ MyMainProject/src/com/suraj/github/Demo2.java
@@ -0,0 +1,16 @@
+package com.suraj.github;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		if(a>b) {
+			System.out.println("Difference is: "+(a-b));
+		}else {
+			System.out.println("sum is: "+(a+b));
+		}
+	}
+
+}
